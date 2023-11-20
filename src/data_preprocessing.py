9449075a93351c358d7125e95585b570e5bd86e6
mkorob <maria.korobeynikova@uzh.ch>
2023-11-20 12:22:53 +0100

    template for data pre-processing

---
+++ src/data_preprocessing.py
@@ -0,0 +1,30 @@
+####Data Pre-processing########
+
+
+#0. Preliminaries
+import requests
+import time
+import pandas as pd
+import json
+from datasets import load_dataset
+import nltk
+nltk.download('punkt')
+
+tokenizer_reg = nltk.RegexpTokenizer(r"\w+")
+
+#1. Import Data
+
+#TODO - file not exported yet
+df = pd.read_csv("data/text_samples.csv")
+
+
+#2. Generate features
+
+#A. N-grams 
+def calculate_ngrams(text):
+    return text
+    
+df_1 = df['text'].apply(calculate_ngrams)
+
+
+
